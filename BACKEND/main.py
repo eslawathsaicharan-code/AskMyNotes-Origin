fd791ea4af6fb73e23699c26c1893c2b8ed2f88b
charan <eslawathsaicharan@gmail.com>
2026-08-14 02:53:36 +0530

    update cors origin

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -19,6 +19,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
